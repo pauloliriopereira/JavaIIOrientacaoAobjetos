c58150825f558a4d13436e41752c1db3b2195404
pauloliriopereira <pauloliriopereira@gmail.com>
2018-01-09 14:29:51 -0200

    Fim da aula 02

--- JavaIIOrientacaoAobjetos/src/aula02/Conta.java
+++ JavaIIOrientacaoAobjetos/src/aula02/Conta.java
@@ -1,5 +1,33 @@
 package aula02;
 
-public class Conta {
-
+public class Conta 
+{
+	protected double saldo;
+	
+	public double getSaldo() 
+	{
+		return this.saldo;
+	}
+	
+	public void deposita( double deposito ) 
+	{
+		this.saldo += deposito;
+	}
+	
+	public void saca( double valor ) 
+	{
+		if( valor <= this.saldo ) 
+		{
+			this.saldo -= valor;
+		}
+		else 
+		{
+			System.out.println( "Saldo induficiente" );
+		}
+	}
+	
+	public void atualiza( double taxa ) 
+	{
+		this.saldo += this.saldo * taxa;
+	}
 }
